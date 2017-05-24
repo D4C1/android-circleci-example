7ad069c32f677c54d14ae86a9e34822b62f76049
Moisés Salas <moises.salas@proto.cool>
2017-05-24 12:26:43 -0700

    circleci example project upload

---
+++ app/src/main/java/cool/proto/android/circleci/example/MainActivity.java
@@ -0,0 +1,13 @@
+package cool.proto.android.circleci.example;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
